811d87326dbf54e8ae2e4fef109eb585d6642b41
super <supeng842499467@gmail.com>
2019-12-29 14:11:54 +0800

    bokeh-quickstart
    bokeh-quickstart

--- dataAnalysis/oh-bokeh/test.py
+++ dataAnalysis/oh-bokeh/test.py
@@ -6,20 +6,37 @@
 # Date:         2019-12-28
 '''
 
+import numpy as np
+
 from bokeh.plotting import figure, output_file, show
+from bokeh.sampledata.stocks import AAPL
 
 # prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+aapl = np.array(AAPL['adj_close'])
+aapl_dates = np.array(AAPL['date'], dtype=np.datetime64)
+
+window_size = 30
+window = np.ones(window_size)/float(window_size)
+aapl_avg = np.convolve(aapl, window, 'same')
 
 # output to static HTML file
-output_file("lines.html")
+output_file("stocks.html", title="stocks.py example")
 
-# create a new plot with a title and axis labels
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+# create a new plot with a datetime axis type
+p = figure(plot_width=800, plot_height=350, x_axis_type="datetime")
 
-# add a line renderer with legend and line thickness
-p.line(x, y, legend="Temp.", line_width=2)
+# add renderers
+p.circle(aapl_dates, aapl, size=4, color='darkgrey', alpha=0.2, legend='close')
+p.line(aapl_dates, aapl_avg, color='navy', legend='avg')
+
+# NEW: customize by setting attributes
+p.title.text = "AAPL One-Month Average"
+p.legend.location = "top_left"
+p.grid.grid_line_alpha = 0
+p.xaxis.axis_label = 'Date'
+p.yaxis.axis_label = 'Price'
+p.ygrid.band_fill_color = "olive"
+p.ygrid.band_fill_alpha = 0.1
 
 # show the results
 show(p)
